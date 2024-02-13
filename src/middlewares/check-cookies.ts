b6b4d26eb1b5f2a1af25367c2ee22682077ac082
victorlopescc <victorlopes.cc@gmail.com>
2024-02-13 17:32:47 -0300

    feat: Cookies middleware

---
+++ src/middlewares/check-cookies.ts
@@ -0,0 +1,14 @@
+import { FastifyRequest, FastifyReply } from "fastify"
+import { knex } from '../database'
+
+export async function checkCookies(request: FastifyRequest, reply: FastifyReply) {
+    const sessionId = request.cookies.sessionId
+
+    if (!sessionId) return reply.status(401).send({ error: 'Unauthorized' })
+
+    const user = await knex('users').where({ session_id: sessionId }).first()
+
+    if (!user) return reply.status(401).send({ error: 'Unauthorized' })
+
+    request.user = user
+}
